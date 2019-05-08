470dd6692558a8a5c78664c119bdde868edf0827
Pastuszka <m.pastuszka@student.mini.pw.edu.pl>
2019-05-08 18:16:59 +0200

    szypko

--- R/tindR.R
+++ R/tindR.R
@@ -3,14 +3,31 @@
 #'@description Select columns from a data frame in an interactive manner
 #'
 #'@param df the data frame to select columns from
+#'@param by optional column to group by
 #'
 #'@export
-tindR <- function(df){
+tindR <- function(df, by=NULL){
   stopifnot(is.data.frame(df))
   dropvector <- c()
+  if(!is.null(by)){
+    par(mfrow=c(1,2))
+    if(is.character(by)){
+      by <- which(colnames(df) == by)
+    }
+  }
   for(i in 1:ncol(df)){
+    if(i == by){
+      dropvector <- c(dropvector, by)
+      next
+    }
     if(is.numeric(df[,i])){
-      boxplot(df[,i])
+      if(!is.null(by)){
+        boxplot(df[,i] ~ df[,by])
+        plot(df[,i], df[,by])
+      }else{
+        boxplot(df[,i])
+        plot(df[,i])
+      }
     }
     print((summary(df[,i])))
     doKeep <- readline(prompt = "Keep this column? [y/n]")
